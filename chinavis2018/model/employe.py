7dfec00883017aeb39e6aa6d1056e784ff4179ad
RandolphCYG <cyg0504@outlook.com>
2019-03-17 21:33:51 +0800

    jieba分词，自定义词典，生成词云（两遍bug未修复）

--- chinavis2018/model/employe.py
+++ chinavis2018/model/employe.py
@@ -11,6 +11,18 @@ import sys
 import re
 import pandas as pd
 from jieba.analyse import *
+import jieba
+import imageio
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, ImageColorGenerator
+
+# 公共路径
+back_coloring_path = "../res/bg.png"  # 设置背景图片路径
+font_path = 'C:/Windows/Fonts/simkai.ttf'  # 为matplotlib设置中文字体路径
+img = "../res/result.png"  # 保存图片(颜色按背景图颜色布局生成)
+stopwords = '../res/stopwords.txt'  # 停用词词表
+subject_dict = '../res/subject_dict.txt'  # 自定义词典
+text_path = '../res/Email.txt'  # 待分析文本路径
 
 def merge(path, csv_name, target_csv):
     # i = 0
@@ -70,7 +82,7 @@ def clean(target_csv, clean_csv):
     # print(read_data)
     read_data.to_csv(clean_csv, index=False)  # 将数据重新写入Email.csv
 def subject_to_txt(clean_txt, clean_csv):
-    f = open(clean_txt, 'a', encoding='utf_8')
+    f = open(clean_txt, 'w', encoding='utf_8')
 
     with open(clean_csv, encoding='utf_8', errors='ignore') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -86,8 +98,64 @@ def word_analysis(clean_txt):
         data = f.read()
     for keyword, weight in extract_tags(data, withWeight=True):
         print('%s %s' % (keyword, weight))
-def subject_word_cloud(clean_txt):
-    pass
+def class_clear_custom(path, subject_dict, stopwords):
+    '''
+    分词&去停用词&加载自定义词典
+    :param path: 待分析文本打开
+    :param subject_dict: 自定义词典设置主词典
+    :param stopwords: 停用词词典
+    :return:
+    '''
+    jieba.load_userdict(subject_dict)
+    mywordlist = []
+    seg_list = jieba.cut(path, cut_all=False)
+    liststr="/ ".join(seg_list)
+    f_stop = open(stopwords, encoding='utf_8', errors='ignore')
+    try:
+        f_stop_text = f_stop.read( )
+    finally:
+        f_stop.close( )
+    f_stop_seg_list=f_stop_text.split('\n')
+    for myword in liststr.split('/'):
+        if not(myword.strip() in f_stop_seg_list) and len(myword.strip())>1:
+            mywordlist.append(myword)
+    # for row in mywordlist:
+    #     print(row)
+    return ''.join(mywordlist)
+
+def subject_word_cloud():
+    '''
+    调分词函数，二次处理主题，生成词云
+    :param back_coloring_path:
+    :param font_path:
+    :param path:
+    :param subject_dict:
+    :param stopwords:
+    :param img:
+    :return:
+    '''
+    back_coloring = imageio.imread(back_coloring_path)# 设置背景图片（图转数组）
+    # 设置词云属性
+    wc = WordCloud(background_color="white",    # 背景
+                   font_path=font_path,   # 字体
+                   max_words=100,    # 最大词数
+                   mask=back_coloring,  # 背景图
+                   max_font_size=80,  # 字体最大值
+                   random_state=42,
+                   width=1000, height=860, margin=2,  # 设置图片默认的大小,
+                   # 但是如果使用背景图片的话,那么保存的图片大小将会按照其大小保存,margin为词语边缘距离
+    )
+    text = open(text_path, encoding='utf_8', errors='ignore').read()
+    text = class_clear_custom(text, subject_dict, stopwords) # 送值，分词，去停用词，加载自定义词典
+
+    wc.generate(text)   # 生成词云 用generate输入全部文本
+    image_colors = ImageColorGenerator(back_coloring)   # 从背景图片生成颜色值
+    plt.figure()
+    plt.imshow(wc.recolor(color_func=image_colors))  # 显示图片
+    plt.axis("off")
+    plt.show()  # 绘制词云
+    wc.to_file(img)    # 保存图片
+
 if __name__ == "__main__":
 
     # email文件整合
@@ -114,8 +182,8 @@ if __name__ == "__main__":
     print("step4.1: 主题词频统计")
     word_analysis(clean_txt)
     print("step4.1: 主题词频统计完成")
-    #
-    # # 词云
-    # print("step4.2: 主题词云分析")
-    # subject_word_cloud(clean_txt)
-    # print("step4.2: 主题词云分析完成")
+
+    # 词云
+    print("step4.2: 主题词云分析")
+    subject_word_cloud()
+    print("step4.2: 主题词云分析完成")
--- chinavis2018/model/subject_word_cloud.py
+++ chinavis2018/model/subject_word_cloud.py
@@ -5,96 +5,76 @@
 # @File    : subject_word_cloud.py
 # @Software: PyCharm
 
-from os import path
-from scipy.misc import imread
-import matplotlib.pyplot as plt
 import jieba
-# jieba.load_userdict("txt\userdict.txt")
-# 添加用户词库为主词典,原词典变为非主词典
+import imageio
+import matplotlib.pyplot as plt
 from wordcloud import WordCloud, ImageColorGenerator
 
-# 获取当前文件路径
-# __file__ 为当前文件, 在ide中运行此行会报错,可改为
-# d = path.dirname('.')
-d = path.dirname(__file__)
+# 公共路径
+back_coloring_path = "../res/bg.png"  # 设置背景图片路径
+font_path = 'C:/Windows/Fonts/simkai.ttf'  # 为matplotlib设置中文字体路径
+img = "../res/result.png"  # 保存图片(颜色按背景图颜色布局生成)
+stopwords = '../res/stopwords.txt'  # 停用词词表
+subject_dict = '../res/subject_dict.txt'  # 自定义词典
+text_path = '../res/Email.txt'  # 待分析文本路径
 
-stopwords = {}
-isCN = 1 #默认启用中文分词
-back_coloring_path = "../res/bg.png" # 设置背景图片路径
-text_path = '../res/Email.txt' #设置要分析的文本路径
-font_path = 'C:/Windows/Fonts/simkai.ttf' # 为matplotlib设置中文字体路径
-stopwords_path = '../res/stopwords.txt' # 停用词词表
-imgname1 = "../res/result1.png" # 保存的图片名字1(只按照背景图片形状)
-imgname2 = "../res/result2.png"# 保存的图片名字2(颜色按照背景图片颜色布局生成)
-
-my_words_list = ['招聘信息总结','面试通知','复试通知','Offer','公司简介','个人资料','个人简历','福利保障','劳动合同','考勤-请假流程','培训邀请'] # 在结巴的词库中添加新词
-
-back_coloring = imread(path.join(d, back_coloring_path))# 设置背景图片
-
-# 设置词云属性
-wc = WordCloud(font_path=font_path,  # 设置字体
-               background_color="white",  # 背景颜色
-               max_words=2000,  # 词云显示的最大词数
-               mask=back_coloring,  # 设置背景图片
-               max_font_size=100,  # 字体最大值
-               random_state=42,
-               width=1000, height=860, margin=2,# 设置图片默认的大小,但是如果使用背景图片的话,那么保存的图片大小将会按照其大小保存,margin为词语边缘距离
-               )
-
-# 添加自己的词库分词
-def add_word(list):
-    for items in list:
-        jieba.add_word(items)
-
-add_word(my_words_list)
-
-text = open(path.join(d, text_path), encoding='utf_8', errors='ignore').read()
-
-def jiebaclearText(text):
+def class_clear_custom(path, subject_dict, stopwords):
+    '''
+    分词&去停用词&加载自定义词典
+    :param path: 待分析文本打开
+    :param subject_dict: 自定义词典设置主词典
+    :param stopwords: 停用词词典
+    :return:
+    '''
+    jieba.load_userdict(subject_dict)
     mywordlist = []
-    seg_list = jieba.cut(text, cut_all=False)
+    seg_list = jieba.cut(path, cut_all=False)
     liststr="/ ".join(seg_list)
-    f_stop = open(stopwords_path)
+    f_stop = open(stopwords, encoding='utf_8', errors='ignore')
     try:
         f_stop_text = f_stop.read( )
-        #f_stop_text=unicode(f_stop_text,'utf-8')
     finally:
         f_stop.close( )
     f_stop_seg_list=f_stop_text.split('\n')
     for myword in liststr.split('/'):
         if not(myword.strip() in f_stop_seg_list) and len(myword.strip())>1:
             mywordlist.append(myword)
+    # for row in mywordlist:
+    #     print(row)
     return ''.join(mywordlist)
 
-if isCN:
-    text = jiebaclearText(text)
+def subject_word_cloud():
+    '''
+    调分词函数，二次处理主题，生成词云
+    :param back_coloring_path:
+    :param font_path:
+    :param path:
+    :param subject_dict:
+    :param stopwords:
+    :param img:
+    :return:
+    '''
+    back_coloring = imageio.imread(back_coloring_path)# 设置背景图片（图转数组）
+    # 设置词云属性
+    wc = WordCloud(background_color="white",    # 背景
+                   font_path=font_path,   # 字体
+                   max_words=100,    # 最大词数
+                   mask=back_coloring,  # 背景图
+                   max_font_size=80,  # 字体最大值
+                   random_state=42,
+                   width=1000, height=860, margin=2,  # 设置图片默认的大小,
+                   # 但是如果使用背景图片的话,那么保存的图片大小将会按照其大小保存,margin为词语边缘距离
+    )
+    text = open(text_path, encoding='utf_8', errors='ignore').read()
+    text = class_clear_custom(text, subject_dict, stopwords) # 送值，分词，去停用词，加载自定义词典
 
-# 生成词云, 可以用generate输入全部文本(wordcloud对中文分词支持不好,建议启用中文分词),
-# 也可以我们计算好词频后使用generate_from_frequencies函数
-wc.generate(text)
-# wc.generate_from_frequencies(txt_freq)
-# txt_freq例子为[('词a', 100),('词b', 90),('词c', 80)]
-# 从背景图片生成颜色值
-image_colors = ImageColorGenerator(back_coloring)
+    wc.generate(text)   # 生成词云 用generate输入全部文本
+    image_colors = ImageColorGenerator(back_coloring)   # 从背景图片生成颜色值
+    plt.figure()
+    plt.imshow(wc.recolor(color_func=image_colors))  # 显示图片
+    plt.axis("off")
+    plt.show()  # 绘制词云
+    wc.to_file(img)    # 保存图片
 
-plt.figure()
-# 以下代码显示图片
-plt.imshow(wc)
-plt.axis("off")
-plt.show()
-# 绘制词云
-
-# 保存图片
-wc.to_file(path.join(d, imgname1))
-
-image_colors = ImageColorGenerator(back_coloring)
-
-plt.imshow(wc.recolor(color_func=image_colors))
-plt.axis("off")
-# 绘制背景图片为颜色的图片
-plt.figure()
-plt.imshow(back_coloring, cmap=plt.cm.gray)
-plt.axis("off")
-plt.show()
-# 保存图片
-wc.to_file(path.join(d, imgname2))
+if __name__ == "__main__":
+    subject_word_cloud()
