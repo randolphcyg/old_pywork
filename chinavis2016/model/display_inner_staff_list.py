6b6923e94493df79c0d187a5c98568741366c7b8
RandolphCYG <cyg0504@outlook.com>
2019-03-23 00:00:48 +0800

    pep8规范审查

--- chinavis2016/model/display_inner_staff_list.py
+++ chinavis2016/model/display_inner_staff_list.py
@@ -6,73 +6,78 @@
 # @Software: PyCharm
 
 import pandas as pd
-import re
 
 # name1 = '../res/chinavis2016_data/a.bassi.csv'
 # name1 = '../res/test.csv'
-name1 = '../res/a.bassi.csv'
+name1 = '../res/chinavis2016_data/a.capaldo.csv'
 
-data = pd.read_csv(name1, encoding='gb18030')
+data = pd.read_csv(name1, encoding='ISO-8859-1')
 # gbk gb18030   # ISO-8859-1编码会导致俄文符号错误显示
 data.fillna(value='0', inplace=True)
 
 # from的display  # , na=False给个缺失值标志
-from_result = data.loc[data['From (address)'].str.contains('O=HACKINGTEAM' or '@hackingteam.it' or '@hackingteam.com', na=False)]
-to_result = data.loc[data['To (address)'].str.contains('O=HACKINGTEAM' or '@hackingteam.it' or '@hackingteam.com', na=False)]
-cc_result = data.loc[data['Cc (address)'].str.contains('O=HACKINGTEAM' or '@hackingteam.it' or '@hackingteam.com', na=False)]
-bcc_result = data.loc[data['Bcc (address)'].str.contains('O=HACKINGTEAM' or '@hackingteam.it' or '@hackingteam.com', na=False)]
+from_result = data.loc[data['From (address)'].str.contains(
+    'O=HACKINGTEAM' or '@hackingteam.it' or '@hackingteam.com', na=False)]
+to_result = data.loc[data['To (address)'].str.contains(
+    'O=HACKINGTEAM' or '@hackingteam.it' or '@hackingteam.com', na=False)]
+cc_result = data.loc[data['Cc (address)'].str.contains(
+    'O=HACKINGTEAM' or '@hackingteam.it' or '@hackingteam.com', na=False)]
+bcc_result = data.loc[data['Bcc (address)'].str.contains(
+    'O=HACKINGTEAM' or '@hackingteam.it' or '@hackingteam.com', na=False)]
 
 # 集合去重得到每个address对应的display
 from_set = set(from_result['From (display)'])
 to_set = set(to_result['To (display)'])
 cc_set = set(cc_result['Cc (display)'])
 bcc_set = set(bcc_result['Bcc (display)'])  # 缺失值给字符串
-result_set = to_set.union(from_set).union(cc_set).union(bcc_set)
-# 四个集合取并集得到全量display
-# result_set = set(result_set)
 
-# print(from_set)
-# print(to_set)
-# print(cc_set)
-# print(bcc_set)
-# print(len(result_set))
+
 def getnamelist(set):
     namelist = []
     for name in set:
-        # p1 = re.compile(r";")
-        # lll = p1.findall(name)
-        # print(lll)
         list_name = name.split(';')
         for item in list_name:
             namelist.append(item)
-    #print(namelist)
     return namelist
-resultlist = getnamelist(from_set) + getnamelist(to_set) + getnamelist(cc_set) + getnamelist(bcc_set)
-# resultlist = getnamelist(from_set).extend(getnamelist(to_set)).extend(getnamelist(cc_set)).extend(getnamelist(bcc_set))
+
+
+resultlist0 = getnamelist(from_set) + getnamelist(to_set) + \
+    getnamelist(cc_set) + getnamelist(bcc_set)
+resultlist0 = set(resultlist0)
+print(len(resultlist0))
+# for name in resultlist0:
+#     print(name)
+resultlist = []
+for name in resultlist0:
+    name = name.replace('\'', '')
+    resultlist.append(name)
+    # print(name)
 resultlist = set(resultlist)
-#print(resultlist)
 print(len(resultlist))
+# for name in resultlist:
+#     print(name)
+clear_resultlist = []
 for name in resultlist:
+    if '@hackingteam.it' in name:
+        name = name.replace('@hackingteam.it', '')
+        clear_resultlist.append(name)
+    elif '@hackingteam.com' in name:
+        name = name.replace('@hackingteam.com', '')
+        clear_resultlist.append(name)
+    elif ' hackingteam.it' in name:
+        name = name.replace(' hackingteam.it', '')
+        clear_resultlist.append(name)
+    else:
+        clear_resultlist.append(name)
+
+clear_resultlist = set(clear_resultlist)
+print(len(clear_resultlist))
+
+for name in clear_resultlist:
     print(name)
 
-# namelist 就是名单
-# 写文件存起来
-# f = open('../res/display_list', 'w', encoding='utf_8', errors='ignore')
-# for name in result_set:
-#     f.write(name+'\n')
-# f.close()
 
-# for name in result_set:
-#     print(name)
-
-
-# from_set = set(from_result['From (address)'])
-# to_set = set(to_result['To (address)'])
-# cc_set = set(cc_result['Cc (address)'])
-# bcc_set = set(bcc_result['Bcc (address)'])  # 缺失值给字符串
-# result_set = to_set.union(from_set).union(cc_set).union(bcc_set)
-#
-# print(len(result_set))
-#
-# for name in from_set:
-#     print(name)
+f = open('../res/display_list', 'w', encoding='utf_8', errors='ignore')
+for name in clear_resultlist:
+    pass
+f.close()
