d18f73871575394030cf14c4af9891c952003a0e
RandolphCYG <cyg0504@outlook.com>
2019-03-28 17:19:12 +0800

    特征提取

---
+++ chinavis2016/model/test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/28 16:25
+# @Author  : Randolph
+# @Email   : cyg0504@outlook.com
+# @File    : test.py
+# @Software: PyCharm
+
+
+
+
+str = '[!bja-272-30493]: issue'
+
+print(str.split(']: ')[1])
