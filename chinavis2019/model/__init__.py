529303db6b5a449c7993b6e7c7471b9d1d43a806
RandolphCYG <cyg0504@outlook.com>
2019-05-16 00:23:53 +0800

    尝试修改结构

---
+++ chinavis2019/model/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/16 0:14
+# @Author  : Randolph
+# @Email   : cyg0504@outlook.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+
---
+++ chinavis2019/res/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/16 0:14
+# @Author  : Randolph
+# @Email   : cyg0504@outlook.com
+# @File    : __init__.py.py
+# @Software: PyCharm
